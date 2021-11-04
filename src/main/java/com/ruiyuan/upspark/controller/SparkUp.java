9c7a3da8e9308517a7cef23e543fdbb3bfb10ba6
origin <1098482060@qq.com>
2021-11-04 14:38:33 +0800

    one

--- src/main/java/com/ruiyuan/upspark/controller/SparkUp.java
+++ src/main/java/com/ruiyuan/upspark/controller/SparkUp.java
@@ -103,7 +103,7 @@ public class SparkUp {
         // 创建yarn的客户端，此类中有杀死任务的方法
         YarnClient yarnClient = YarnClient.createYarnClient();
 
-        //ok ok ok jujuju
+        //push  commit
 
         // 初始化yarn的客户端
         yarnClient.init(cf);
